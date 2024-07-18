74ce9e1283272ebabdcf6f0c813481d3e5d47249
henrydatei <31316025+henrydatei@users.noreply.github.com>
2024-07-18 15:08:49 +0200

    fix #21, QuoteOrder again

--- classes/OrderResponse.py
+++ classes/OrderResponse.py
@@ -1,10 +1,10 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Optional
 
 @dataclass(frozen=True)
 class OrderResponse:
     success: bool
-    reason: Optional[str]
-    orderGuid: Optional[str]
-    needsTfaReAuth: Optional[bool]
-    needsPasswordReAuth: Optional[bool]
+    reason: Optional[str] = field(default=None)
+    orderGuid: Optional[str] = field(default=None)
+    needsTfaReAuth: Optional[bool] = field(default=None)
+    needsPasswordReAuth: Optional[bool] = field(default=None)
